e53b4a9df361d4994b7ae7f9e562ae56ea630cbf
HIMANSHU KUMAR MISHRA <41019847@BSESRPL.RelianceEnergy.com>
2024-04-16 19:12:46 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//All Listings
+Route::get('/', [ListingController::class,'index']);
+
+//Show create form
+Route::get('/listings/create', [ListingController::class,'create']);
+
+//Store Listing Data
+Route::post('/listings',[ListingController::class,'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+//Edit Submit to Update
+Route::put('/listings/{listing}',[ListingController::class,'update']);
+
+//Delete Lisitng
+Route::delete('/listings/{listing}',[ListingController::class,'destroy']);
+
+//Single Listing
+Route::get('/listings/{listing}',[ListingController::class,'show']);
+
+
+
